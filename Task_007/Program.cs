207bc6e0d2d20963a0aa650e3d69c946af0cb96e
nudel98@ya.ru <ameon@ya.ru>
2023-06-25 10:21:08 +0500

    update

---
+++ Task_007/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 7. Выполнить конкатенацию и интерполяцию.
+
+// 1. Конкатенация
+string name = "Дмитрий";
+int age = 25;
+
+string output = "Меня зовут " + name + " и мне " + age + " лет.";
+Console.WriteLine(output);
+
+// 2. Интерполяция
+string name2 = "Дмитрий";
+int age2 = 25;
+
+string output2 = $"Меня зовут {name2} и мне {age2} лет.";
+Console.WriteLine(output2);
---
+++ Task_008/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 8. Взять число и возвести его в квадрат.
+Console.Write("Введите число, квадрат которого вы хотите увидеть -> ");
+int num = Convert.ToInt32(Console.ReadLine());
+int quad = num * num;
+System.Console.WriteLine($"Квадрат числа {num} -> {quad}");
